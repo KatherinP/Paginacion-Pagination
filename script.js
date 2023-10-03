a408c0298bed4421f3bd2dfd6d12784c7a8f6f3a
Yulieth Caterin <ycpaezch@gmail.com>
2023-10-03 16:45:20 -0500

    Creacion paginacion - Se inicia con la creacion de la paginacion - Se agregan estilos - Se agrega JS para la seleccion de elementos - Se agrega JS para establecer el paso inicial

---
+++ script.js
@@ -0,0 +1,9 @@
+// Selecting DOM elements/seleccion elementos
+const startBtn = document.querySelector("#startBtn"),
+endBtn = document.querySelector("endBtn"),
+prevNext = document.querySelectorAll("prevNext"),
+numbers = document.querySelectorAll(".links");
+
+// setting an initial step/estableciendo un paso inicial
+let currentStep = 0;
+

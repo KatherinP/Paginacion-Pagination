c5020c99c0391a0634089b640a6a6f91a7b3efd4
Yulieth Caterin <ycpaezch@gmail.com>
2023-10-06 18:12:12 -0500

    Modificaciones - Se agrega funcion para actualizar los estados de los botones - Se agrega detectores de eventos a los enlaces numericos - Se agrega detectores de eventos a los botones anterior y siguiente

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
 // Selecting DOM elements/seleccion elementos
 const startBtn = document.querySelector("#startBtn"),
-endBtn = document.querySelector("endBtn"),
-prevNext = document.querySelectorAll("prevNext"),
-numbers = document.querySelectorAll(".links");
+    endBtn = document.querySelector("#endBtn"),
+    prevNext = document.querySelectorAll(".prevNext"),
+    numbers = document.querySelectorAll(".link");
 
 // setting an initial step/estableciendo un paso inicial
 let currentStep = 0;
 
+// Function to update the button states/Función para actualizar los estados de los botones.
+const updateBtn =() => {
+    // If the are at the last step/si están en el último pas
+    if(currentStep === 4) {
+        endBtn.disabled = true;
+        prevNext[1].disabled = true;
+    }else if(currentStep === 0){
+        // If the are at the first step/Si están en el primer paso.
+        startBtn.disabled = true;
+        prevNext[0].disabled = true;
+    }else {
+        endBtn.disabled = false;
+        prevNext[1].disabled = false;
+        startBtn.disabled = false;
+        prevNext[0].disabled = false;
+    }
+};
+
+// Add event listeners to the number links/agregar detectores de eventos a los enlaces numéricos
+numbers.forEach((number, numIndex) => {
+    number.addEventListener("click", (e) => {
+        e.preventDefault();
+        // set the current step to the clicked number link/establezca el paso actual en el enlace del número en el que se hizo click
+        currentStep = numIndex;
+        // remove the "active" class from the previously active number link/eliminar la clase "activa" del enlace de número previamente activo
+        document.querySelector(".active").classList.remove("active");
+        // add the "active" class to the clicked number link/agregue la clase "activa" al enlace del número en el que se hizo click
+        number.classList.add("active");
+        updateBtn(); //update the button states/actualizar los estados del botón
+    });
+});
+
+// Add event listeners to the "Previous" and "Next buttons"/agregue detectores de eventos a los botones "Anterior" y "Siguiente"
+prevNext.forEach(button => {
+    button.addEventListener("click", (e) => {
+        // increment or decrement the current stepp based on the button clicked/incrementar o disminuir el paso actual según el botón en el que se hizo click
+        currentStep += e.target.id === "next" ? 1 : -1;
+        numbers.forEach((number, numIndex) => {
+            // Toggle the "active" class on the number links based on the current step/Alternar la clase "activa" en los enlaces numéricos según el paso actual
+            number.classList.toggle("active", numIndex === currentStep);
+            updateBtn(); //update the button states/actualizar los estados del botón
+        });
+    });
+});
